09f2b3ee20310d9ee3392efb91b9ba866bf04fbb
rachmadaniHaryono <foreturiga@gmail.com>
2018-02-06 21:07:53 +0800

    chg: dev: [server] remove unused code

--- google_images_download/server.py
+++ google_images_download/server.py
@@ -1,22 +1,19 @@
 """Server module."""
 from logging.handlers import TimedRotatingFileHandler
-from urllib.parse import urlencode, parse_qs, urlparse
-import logging  # pylint: disable=ungrouped-imports
+import logging
 import os
 import shutil
 import tempfile
 
 from appdirs import user_data_dir
-from flask import Flask, render_template, request, url_for, flash, send_from_directory
+from flask import Flask, request, flash, send_from_directory
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-from flask_bootstrap import Bootstrap
-from flask_restless import APIManager  # pylint: disable=import-error
 import click
 import structlog
 
-from google_images_download.forms import IndexForm, FindImageForm
-from google_images_download import models, pagination, admin, api, sha256
+from google_images_download import models, admin, api, sha256
+from google_images_download.forms import FindImageForm
 
 
 app = Flask(__name__)
@@ -31,48 +28,6 @@ class ImageURLSingleView(BaseView):
         entry = models.ImageURL.query.filter_by(url=search_url).one_or_none()
         return self.render(
             'google_images_download/image_url_view.html', entry=entry, search_url=search_url)
-
-
-# @app.route('/')
-def index():
-    """Get Index page."""
-    form = IndexForm(request.args)
-    search_query = form.query.data
-    limit = form.limit.data
-    entry = None
-    render_template_kwargs = {'form': form, 'entry': entry}
-    page = form.page.data if form.page.data else 1
-    if not search_query:
-        return render_template('index.html', **render_template_kwargs)
-
-    sq_m, _ = models.SearchQuery.get_or_create_from_query(search_query, page - 1)
-    if not sq_m.match_results:
-        sq_m.get_match_results()
-    app.logger.debug(
-        '%s match(s) found for [%s] page:%s', len(sq_m.match_results), sq_m.query, page)
-    entry = sq_m
-
-    entry_match_results = entry.match_results
-    if limit and limit < len(entry.match_results):
-        entry_match_results = entry.match_results[:limit]
-    render_template_kwargs['entry_match_results'] = entry_match_results
-
-    def return_page_url(page_input):
-        """Return page url."""
-        url_query = parse_qs(urlparse(request.url).query)
-        url_query['page'] = [page_input]
-        new_query = urlencode(url_query, True)
-        parsed_url = urlparse(url_for('index'))
-        parsed_url = parsed_url._replace(query=new_query)
-        return parsed_url.geturl()
-
-    app.logger.debug('Limit: %s', limit)
-    render_template_kwargs.update({
-        'form': form, 'entry': entry,
-        'pagination': pagination.Pagination(page, return_page_url),
-        'limit': limit
-    })
-    return render_template('index.html', **render_template_kwargs)
 
 
 @app.route('/t/<path:filename>')
@@ -96,20 +51,16 @@ def create_app(script_info=None):  # pylint: disable=unused-argument
         directory = 'log'
         if not os.path.exists(directory):
             os.makedirs(directory)
-        default_log_file = os.path.join(
-            directory, 'google_images_download_server.log')
-        file_handler = TimedRotatingFileHandler(
-            default_log_file, 'midnight')
+        default_log_file = os.path.join(directory, 'google_images_download_server.log')
+        file_handler = TimedRotatingFileHandler(default_log_file, 'midnight')
         file_handler.setLevel(logging.WARNING)
-        file_handler.setFormatter(
-            logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
+        file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
         app.logger.addHandler(file_handler)
 
     app_admin = Admin(app, name='google image download', template_mode='bootstrap3')
     app_admin.add_view(ModelView(models.SearchQuery, models.db.session))
     app_admin.add_view(ModelView(models.MatchResult, models.db.session))
     app_admin.add_view(ModelView(models.ImageURL, models.db.session))
-    Bootstrap(app)
     return app
 
 
@@ -235,36 +186,28 @@ def run(host='127.0.0.1', port=5000, debug=False, reloader=False, threaded=False
     directory = 'log'
     if not os.path.exists(directory):
         os.makedirs(directory)
-    default_log_file = os.path.join(
-        directory, 'google_images_download_server.log')
-    file_handler = TimedRotatingFileHandler(
-        default_log_file, 'midnight')
+    default_log_file = os.path.join(directory, 'google_images_download_server.log')
+    file_handler = TimedRotatingFileHandler(default_log_file, 'midnight')
     file_handler.setLevel(logging.WARNING)
-    file_handler.setFormatter(
-        logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
+    file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
     app.logger.addHandler(file_handler)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gid_debug.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     if debug:
         app.config['DEBUG'] = True
         app.config['LOGGER_HANDLER_POLICY'] = 'debug'
-        app.config['SECRET_KEY'] = os.getenv('DDG_SERVER_SECRET_KEY') or \
-            os.urandom(24)
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gid_debug.db'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SECRET_KEY'] = os.getenv('DDG_SERVER_SECRET_KEY') or os.urandom(24)
         app.config['WTF_CSRF_ENABLED'] = False
         models.db.init_app(app)
         app.app_context().push()
         models.db.create_all()
         logging.basicConfig(level=logging.DEBUG)
 
-    api_manager = APIManager(app, flask_sqlalchemy_db=models.db)
-    api_manager.create_api(models.SearchQuery, methods=['GET'])
-    api_manager.create_api(models.MatchResult, methods=['GET'])
-
-    admin_templ = 'google_images_download/index.html'
+    # flask-admin
     app_admin = Admin(
         app, name='Google images download', template_mode='bootstrap3',
-        index_view=admin.HomeView(name='Home', template=admin_templ, url='/'))
+        index_view=admin.HomeView(name='Home', template='google_images_download/index.html', url='/'))  # NOQA
     app_admin.add_view(FromFileSearchImageView(name='Image Search', endpoint='f'))
     app_admin.add_view(ImageURLSingleView(name='Image Viewer', endpoint='u'))
     app_admin.add_view(admin.SearchQueryView(models.SearchQuery, models.db.session, category='History'))  # NOQA
@@ -277,10 +220,6 @@ def run(host='127.0.0.1', port=5000, debug=False, reloader=False, threaded=False
     # app_admin.add_view(admin.SearchImagePageView(models.SearchImagePage, models.db.session, category='History'))  # NOQA
     app_admin.add_view(admin.TextMatchView(models.TextMatch, models.db.session, category='History'))  # NOQA
     app_admin.add_view(admin.MainSimilarResultView(models.MainSimilarResult, models.db.session, category='History'))  # NOQA
-    model_list = []  # NOTE: may be used later
-    for model_item in model_list:
-        app_admin.add_view(ModelView(model_item, models.db.session))
-    Bootstrap(app)
 
     app.run(
         host=host, port=port,
