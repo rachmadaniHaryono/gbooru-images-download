5c5ff34f1fa58b62c349cc15c95d6897ceff790d
rachmadaniHaryono <foreturiga@gmail.com>
2018-02-28 06:28:05 +0800

    fix: test: test import

--- tests/test_google_images_download.py
+++ tests/test_google_images_download.py
@@ -3,6 +3,6 @@
 
 def test_rename_basename():
     """test."""
-    from google_images_download.google_images_download import rename_basename
+    from google_images_download.google_images_download_v0 import rename_basename
     assert '/home/user/Downloads/dog.jpg' == \
         rename_basename('/home/user/Downloads/cat.jpg', 'dog')
