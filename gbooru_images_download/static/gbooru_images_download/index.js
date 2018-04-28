4ac54a5a3a48b1854dfaa7d572e0080e3c730841
rachmadaniHaryono <foreturiga@gmail.com>
2018-04-28 23:50:06 +0800

    new: dev: use masonry for image figures

--- gbooru_images_download/static/gbooru_images_download/index.js
+++ gbooru_images_download/static/gbooru_images_download/index.js
@@ -14,3 +14,10 @@ $(".hide-btn").click(function(e) {
       },
   );
 });
+
+var $grid = $('.grid').imagesLoaded( function() {
+  // init Masonry after all images have loaded
+  $grid.masonry({
+    itemSelector: '.grid-item',
+  });
+});
