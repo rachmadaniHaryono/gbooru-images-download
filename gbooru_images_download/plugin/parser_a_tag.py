b94671923f4a26704807bde5882696c7686ed794
rachmadaniHaryono <foreturiga@gmail.com>
2018-06-29 07:50:44 +0800

    chg: dev: log skipped img_src and href

--- gbooru_images_download/plugin/parser_a_tag.py
+++ gbooru_images_download/plugin/parser_a_tag.py
@@ -1,7 +1,12 @@
 from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
+import structlog
+
 from gbooru_images_download import models, api
+
+
+log = structlog.getLogger(__name__)
 
 
 class ParserPlugin(api.ParserPlugin):
@@ -13,12 +18,16 @@ class ParserPlugin(api.ParserPlugin):
         mrs = list(pp.plugin_object.get_match_results(text, session, url=url))
         list(map(session.add, mrs))
         session.commit()
+        skipped_hrefs = []
         for a_tag in a_tags:
             href = a_tag.attrs.get('href', None)
             if href:
                 if href.startswith(('#', '.')) and url:
                     href = urljoin(url, href)
                 elif href.startswith(('#', '.')) and not url:
-                    continue
+                    skipped_hrefs.append(href)
                 url_model = models.get_or_create(session, models.Url, value=href)[0]
                 yield models.get_or_create(session, models.MatchResult, url=url_model)[0]
+        if skipped_hrefs:
+            log.debug('url', v=url)
+            list(map(lambda x: log.debug('href', v=x), skipped_hrefs))
--- gbooru_images_download/plugin/parser_a_tag_on_img_tag.py
+++ gbooru_images_download/plugin/parser_a_tag_on_img_tag.py
@@ -1,7 +1,12 @@
 from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
+import structlog
+
 from gbooru_images_download import models, api
+
+
+log = structlog.getLogger(__name__)
 
 
 class ParserPlugin(api.ParserPlugin):
@@ -10,24 +15,31 @@ class ParserPlugin(api.ParserPlugin):
         soup = BeautifulSoup(text, 'html.parser')
         a_tags = soup.select('a')
         session.commit()
+        skipped_hrefs = []
+        skipped_img_src = []
         for a_tag in a_tags:
             href = a_tag.attrs.get('href', None)
             if href.startswith(('#', '.')):
                 href = urljoin(url, href)
             elif href.startswith(('#', '.')) and not url:
-                continue
+                skipped_hrefs.append(href)
             if not href:
-                continue
+                skipped_hrefs.append(href)
             for img_tag in a_tag.select('img'):
                 img_src = img_tag.get('src', None)
                 if img_src:
                     if img_src.startswith(('#', '.')):
                         img_src = urljoin(url, img_src)
                     elif img_src.startswith(('#', '.')) and not url:
-                        continue
+                        skipped_img_src.append(img_src)
                     url_model = models.get_or_create(
                         session, models.Url, value=href)[0]
                     img_url_model = models.get_or_create(
                         session, models.Url, value=img_src)[0]
                     yield models.get_or_create(
                         session, models.MatchResult, url=url_model, thumbnail_url=img_url_model)[0]
+
+        if any(skipped_hrefs, skipped_img_src):
+            log.debug('url', v=url)
+            list(map(lambda x: log.debug('href', v=x), skipped_hrefs))
+            list(map(lambda x: log.debug('img src:', v=x), skipped_img_src))
